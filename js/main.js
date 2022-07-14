f51ecf9b31994c3c8994c64e4ce07f7d4fa17811
browin lopez <browinsantiago@gmail.com>
2022-07-14 13:59:23 -0500

    actualizacion
    formulario de inicio de sesion
    cambio en el input contraseña
    cambio en el select de empresa

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
 const inputs = document.querySelectorAll(".input");
 
-
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
+function addcl() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
 }
 
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
+function remcl() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
 }
 
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+inputs.forEach((input) => {
+  input.addEventListener("focus", addcl);
+  input.addEventListener("blur", remcl);
 });
 
 $(document).ready(function () {
-
-	$('.owl-carousel').owlCarousel({
-		loop: true,
-		autoplay: true,
-		autoplayTimeout: 3000,
-		autoplayHoverPause: true,
-		items: 1
-	});
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    items: 1,
+  });
 });
+
+const iconeye = document.querySelector(".icon-eye");
+console.log(iconeye);
+
+function SeePassword() {
+  var contraseña = document.getElementById("password");
+  var icon = document.getElementById("icon-eye-js");
+  if (contraseña.type == "password") {
+    contraseña.type = "text";
+	icon.className = "fas fa-eye";
+  } else if (contraseña.type == "text") {
+    contraseña.type = "password";
+	icon.className = "fas fa-eye-slash";
+  }
+}
